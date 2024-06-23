34593b8e639c6ee12745f7236d9e9378ecaee72f
Thanatermesis <thanatermesis@gmail.com>
2024-06-23 17:54:25 -0500

    added tabnine template

--- plugins.lua
+++ plugins.lua
@@ -151,6 +151,78 @@ local plugins = {
 
 
   -- }}}
+  -- nvim-cmp with TabNine code AI {{{
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   event = "InsertEnter",
+  --   init = function()
+  --     -- load special mappings that also shows up on which-key
+  --     require("core.utils").load_mappings "cmp"
+  --   end,
+  --   dependencies = {
+  --     {
+  --       -- snippet plugin
+  --       "L3MON4D3/LuaSnip",
+  --       dependencies = "rafamadriz/friendly-snippets",
+  --       opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+  --       config = function(_, opts)
+  --         require("plugins.configs.others").luasnip(opts)
+  --       end,
+  --     },
+  --     -- tabnine configuration:
+  --     {
+  --       "tzachar/cmp-tabnine",
+  --       build = "./install.sh",
+  --       config = function()
+  --         local tabnine = require "cmp_tabnine.config"
+  --         tabnine:setup {
+  --           max_lines = 1000,
+  --           max_num_results = 20,
+  --           sort = true,
+  --           run_on_every_keystroke = true,
+  --           snippet_placeholder = '..',
+  --           show_prediction_strength = false
+  --         } -- put your options here
+  --       end,
+  --     },
+  --
+  --     -- autopairing of (){}[] etc
+  --     -- {
+  --     --   "windwp/nvim-autopairs",
+  --     --   opts = {
+  --     --     fast_wrap = {},
+  --     --     disable_filetype = { "TelescopePrompt", "vim" },
+  --     --   },
+  --     --   config = function(_, opts)
+  --     --     require("nvim-autopairs").setup(opts)
+  --     --
+  --     --     -- setup cmp for autopairs
+  --     --     local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+  --     --     require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  --     --   end,
+  --     -- },
+  --
+  --     -- cmp sources plugins
+  --     {
+  --       "saadparwaiz1/cmp_luasnip",
+  --       "hrsh7th/cmp-nvim-lua",
+  --       "hrsh7th/cmp-nvim-lsp",
+  --       "hrsh7th/cmp-buffer",
+  --       "hrsh7th/cmp-path",
+  --     },
+  --   },
+  --
+  --   opts = function()
+  --     return require "custom.configs.cmp"
+  --     -- TODO: add to "sources" entry:
+  --     -- { name = "cmp_tabnine" },
+  --   end,
+  --   config = function(_, opts)
+  --     require("cmp").setup(opts)
+  --   end,
+  -- },
+
+  -- }}}
   -- nvim-cmp (main conf needs overwrite special settings for copilot) {{{
   {
     "hrsh7th/nvim-cmp",
